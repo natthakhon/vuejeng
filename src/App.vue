d08e2d5ea5b20f20c31213210a9d2d4274fda706
Natthakhon Laosurasuntorn <natthakhonl@dpa.com.sg>
2025-05-13 12:48:24 +0700

    bootstrap and router

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <input v-model="option" placeholder="Enter something" />
-  <button @click="myfunction">Click me</button>
-  <HelloWorld 
-    :msg="mymessage" 
-    :mylist="myiiemlist"
-  />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/test">Test</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      mymessage:"hello world",
-      myiiemlist: [],
-      option: ''
-    }
-  },
-  methods:{
-    myfunction(){
-      if (this.option == '1'){
-        this.myiiemlist= ['Item 1' , 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7']
-      }
-      else{
-        this.myiiemlist= []
-      }
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -43,6 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container-fluid">
+        <h1>Three equal width columns</h1>
+        <p>Note: Try to add a new div with class="col" inside the row class - this will create four equal-width columns.</p>
+        <div class="row">
+            <div class="col p-3 bg-primary text-white">.col</div>
+            <div class="col p-3 bg-dark text-white">.col</div>
+            <div class="col p-3 bg-primary text-white">.col</div>
+        </div>
+        <div class="row mt-2">
+            <div class="col p-3 bg-primary text-white">.col</div>
+            <div class="col p-3 bg-dark text-white">.col</div>
+            <div class="col p-3 bg-primary text-white">.col</div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-sm-4 bg-primary">
+                <div class="row mt-2">
+                    <div class="col-sm-2 bg-primary text-white">.col</div>
+                    <div class="col-sm-6 bg-dark text-white">.col</div>
+                    <div class="col-sm-4 bg-primary text-white">.col</div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="row mt-2">
+                    <div class="col-sm-4 bg-primary">
+                        <label for="email" class="form-label">Email:</label>
+                    </div>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TestView'
+}
+</script>
